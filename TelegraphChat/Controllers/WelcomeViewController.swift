cc4730752127f8552c25e7dfcee11f4d19dbbff8
beratridvanasilturk <br.asltrk@gmail.com>
2023-05-24 23:53:18 +0300

    AnimationTextCreatedWithPod

--- TelegraphChat/Controllers/WelcomeViewController.swift
+++ TelegraphChat/Controllers/WelcomeViewController.swift
@@ -5,24 +5,28 @@
 //  Created by Berat Ridvan Asilturk 23/05/2023.
 //
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    ///Pod icin label'i ClTypingLabel olarak ayarladik
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-        titleLabel.text = ""
-        var chracterIndex = 0.0
-        let titleText = "⚡️TelegraphChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.5 * chracterIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            chracterIndex += 1
-        }
-        ///Text'i animasyon haline getirir, ve bunu her bir harf icin ayni anda yapmasini engellemek adina da chracterIndex tanimladik .
+
+        titleLabel.text = "⚡️TelegraphChat"
+        
+//        titleLabel.text = ""
+//        var chracterIndex = 0.0
+//        let titleText = "⚡️TelegraphChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.5 * chracterIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            chracterIndex += 1
+//        }
+        ///Text'i normal bicimde herhangi bir pos kullanmadan animasyon haline getirir, ve bunu her bir harf icin ayni anda yapmasini engellemek adina da chracterIndex tanimladik .
        
     }
 }
