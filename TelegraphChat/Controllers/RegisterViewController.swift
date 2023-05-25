fc4282dd27dc16f102607634201ab45890ac5023
beratridvanasilturk <br.asltrk@gmail.com>
2023-05-26 01:07:49 +0300

    Password Authentication "Register" Created

--- TelegraphChat/Controllers/RegisterViewController.swift
+++ TelegraphChat/Controllers/RegisterViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-    @IBAction func registerPressed(_ sender: UIButton) {
-    }
     
+        
+        @IBAction func registerPressed(_ sender: UIButton) {
+            
+            if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                if let e = error {
+                    print(e)
+                } else {
+                    /// Else ise ChatViewController'a git komutu :
+                    self!.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+            ///firebase register butonu tetikleme kodu, Password authentication icin.
+        }
+    }
 }
