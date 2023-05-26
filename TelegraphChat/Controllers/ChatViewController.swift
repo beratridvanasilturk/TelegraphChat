82c060df9dd07df4a02602788df5de2a692de079
beratridvanasilturk <br.asltrk@gmail.com>
2023-05-26 17:35:50 +0300

    Log Out Button Added
    LogOut button added and back button hided in the chatViewController.

--- TelegraphChat/Controllers/ChatViewController.swift
+++ TelegraphChat/Controllers/ChatViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -14,7 +17,26 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        ///Login olduktan sonra log out butonuyla cikis yapilabilecegi icin back butonunu bu ekranda islevsiz kalir ve bu kod ile back butonu devre disi birakilir
+        title = "Telegraph Chat"
+        
 
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        
+        do {
+          try Auth.auth().signOut()
+            
+            navigationController?.popToRootViewController(animated: true)
+            ///Sign out basarili oldugunda giris olan kok root ekrana doner
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
