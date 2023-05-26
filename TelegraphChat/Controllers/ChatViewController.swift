29eb8fa8c98fb686e6919db4cc912754a08e8422
beratridvanasilturk <br.asltrk@gmail.com>
2023-05-27 02:17:50 +0300

    Messages Table View Created

--- TelegraphChat/Controllers/ChatViewController.swift
+++ TelegraphChat/Controllers/ChatViewController.swift
@@ -15,12 +15,25 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    
+    var messages: [Message] = [
+        Message(sender: "Deneme1@mail.com", body: "Hey"),
+        Message(sender: "Deneme2@mail.com", body: "Hey man!"),
+        Message(sender: "Deneme1@mail.com", body: "What's up mate?"),
+        Message(sender: "Deneme2@mail.com", body: "Everthing's perfect bro where are you?"),
+        Message(sender: "Deneme1@mail.com", body: "Umm... I don't know i just see a huge shark"),
+        Message(sender: "Deneme2@mail.com", body: "What? a Shark? Ahh you are joking me..")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        //
+        
         navigationItem.hidesBackButton = true
         ///Login olduktan sonra log out butonuyla cikis yapilabilecegi icin back butonunu bu ekranda islevsiz kalir ve bu kod ile back butonu devre disi birakilir
-        title = "Telegraph Chat"
+        title = Constants.appName
         
 
     }
@@ -44,3 +57,21 @@ class ChatViewController: UIViewController {
     
 
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    ///Mesajlasma kadar table view row olusturur
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        /// sirasiyla mesajlari texte atar
+        return cell
+    }
+    
+    
+}
+
+
